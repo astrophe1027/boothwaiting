dbd84012ef174c62ff1a3af634243723a0954cb2
astrophe1027 <junghyunjin20@gmail.com>
2026-06-08 21:44:27 +0900

    입장, 퇴장기 보안 강화

--- src/main/java/kr/hs/sen/bangsan/boothwaiting/controller/EntryController.java
+++ src/main/java/kr/hs/sen/bangsan/boothwaiting/controller/EntryController.java
@@ -3,6 +3,7 @@ package kr.hs.sen.bangsan.boothwaiting.controller;
 import kr.hs.sen.bangsan.boothwaiting.service.AccountService;
 import kr.hs.sen.bangsan.boothwaiting.service.WaitingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,18 @@ public class EntryController {
     @Autowired
     private WaitingService waitingService;
 
+    @Value("${booth.client-password}")
+    private String clientPassword;
+
     @PostMapping(path="/api/entry")
-    public synchronized ResponseEntity<Map<String, String>> enter(@RequestParam(value = "token", defaultValue = "") String token) {
+    @CrossOrigin(origins = "*")
+    public synchronized ResponseEntity<Map<String, String>> enter(@RequestParam(value = "token", defaultValue = "") String token, @RequestHeader(value = "X-Client-Password", defaultValue = "") String password) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
-        Map<String, String> response = new HashMap<>();
-        response.put("message", accountService.enter(waitingService.getStudentIdByToken(token)));
-        return new ResponseEntity<>(response, headers, HttpStatus.OK);
+        if (!clientPassword.equals(password)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).headers(headers)
+                    .body(Map.of("message", "인증되지 않은 요청입니다."));
+        }
+        return new ResponseEntity<>(Map.of("message", accountService.enter(waitingService.getStudentIdByToken(token))), headers, HttpStatus.OK);
     }
 }
--- src/main/java/kr/hs/sen/bangsan/boothwaiting/controller/ExitController.java
+++ src/main/java/kr/hs/sen/bangsan/boothwaiting/controller/ExitController.java
@@ -3,25 +3,31 @@ package kr.hs.sen.bangsan.boothwaiting.controller;
 import kr.hs.sen.bangsan.boothwaiting.service.AccountService;
 import kr.hs.sen.bangsan.boothwaiting.service.WaitingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ExitController {
+
     @Autowired
     private AccountService accountService;
-    @Autowired
-    private WaitingService waitingService;
 
-    @DeleteMapping(path="/api/entry")
-    public synchronized ResponseEntity<Map<String, String>>  exit(@RequestParam(name="studentId", defaultValue = "0") int studentId) {
-        Map <String, String>  response = new HashMap<>();
-        response.put("message", accountService.exit(studentId));
-        return ResponseEntity.ok(response);
+    @Value("${booth.client-password}")
+    private String clientPassword;
+
+    @DeleteMapping(path = "/api/entry")
+    public ResponseEntity<Map<String, String>> exit(@RequestParam(name = "studentId", defaultValue = "0") int studentId, @RequestHeader(value = "X-Client-Password", defaultValue = "") String password) {
+        if (!clientPassword.equals(password)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "인증되지 않은 요청입니다."));
+        }
+        return ResponseEntity.ok(Map.of("message", accountService.exit(studentId)));
     }
 }
